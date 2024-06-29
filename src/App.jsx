2218d953680ed7d452577957ea4350ae795b8462
mezealot24 <wiriya.on@gmail.com>
2024-06-29 16:00:21 +0700

    Simon V1.0

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<button>1</button>
+			<button>2</button>
+			<button>3</button>
+			<button>4</button>
+		</>
+	);
+}
+
+export default App;
